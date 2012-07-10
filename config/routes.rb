13c6ac26297cff9f913d4146b592d855c4f626cd
jasonm23 <jasonm23@gmail.com>
2012-07-10 22:20:59 +1000

    add issue fields to select

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 ActionController::Routing::Routes.draw do |map|
-  map.contributed_find 'contributed/:id', :controller => 'contributed', :action => 'find'
+  map.contributed_find 'contributed/:id.json', :controller => 'contributed', :action => 'find'
 end
