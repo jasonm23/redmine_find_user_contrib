8790b3033373e0208b5696c8a8ea0396d7532da0
Jasonm23 <jasonm23@gmail.com>
2012-07-10 22:29:00 +1000

    limit result fields to issues fields

--- app/controllers/contributed_controller.rb
+++ app/controllers/contributed_controller.rb
@@ -1,17 +1,17 @@
 class ContributedController < ApplicationController
   unloadable
-
+  
   def find
+
     id = params[:id]
+    field_list = "id, tracker_id, project_id, subject, description, due_date, category_id, status_id, assigned_to_id, priority_id, fixed_version_id, author_id, lock_version, created_on, updated_on, start_date, done_ratio, estimated_hours, parent_id, root_id, lft, rgt, is_private"
 
-    # Issue(id: integer, tracker_id: integer, project_id: integer, subject: string, description: text, due_date: date, category_id: integer, status_id: integer, assigned_to_id: 
-    # integer, priority_id: integer, fixed_version_id: integer, author_id: integer, lock_version: integer, created_on: datetime, updated_on: datetime, start_date: date, 
-    # done_ratio: integer, estimated_hours: float, parent_id: integer, root_id: integer, lft: integer, rgt: integer, is_private: boolean
+    @issues = Issue.find_by_sql("select #{field_list} from issues,journals where issues.author_id = #{id} or issues.assigned_to_id = #{id} or (issues.id = journals.journalized_id and journals.user_id = #{id})")
 
-    @issues = Issue.find_by_sql("select * from issues,journals where issues.author_id = #{id} or issues.assigned_to_id = #{id} or (issues.id = journals.journalized_id and journals.user_id = #{id})")
     respond_to do |format|
       format.json { send_data( @issues.to_json ) }
     end
+
   end
 
 end
