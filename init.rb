df47685f842c04159a690ef8fb5f9b5e7ec1624a
Jasonm23 <jasonm23@gmail.com>
2012-07-10 18:48:31 +1000

    added tests

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_find_user_contrib do
   name 'Redmine Find User Contrib plugin'
-  author 'Author name'
+  author 'Jason Milkins'
   description 'Find issues, a user contributed to'
   version '0.0.1'
   url 'https://github.com/jasonm23/redmine_find_user_contrib'
--- test/functional/contibuted_controller_test.rb
+++ test/functional/contibuted_controller_test.rb
@@ -5,4 +5,29 @@ class ContibutedControllerTest < ActionController::TestCase
   def test_truth
     assert true
   end
+
+  def test_access_user_model
+    assert User.all.is? Array
+  end
+
+  def test_user_structure
+    assert User.inspect == "User(id: integer, login: string, hashed_password: string, firstname: string, lastname: string, mail: string, admin: boolean, status: integer, last_login_on: datetime, language: string, auth_source_id: integer, created_on: datetime, updated_on: datetime, type: string, identity_url: string, mail_notification: string, salt: string)"
+  end
+    
+  def test_access_issue_model
+    assert Issue.all.is? Array
+  end
+
+  def test_issue_structure
+    assert Issue.inspect == "Issue(id: integer, tracker_id: integer, project_id: integer, subject: string, description: text, due_date: date, category_id: integer, status_id: integer, assigned_to_id: integer, priority_id: integer, fixed_version_id: integer, author_id: integer, lock_version: integer, created_on: datetime, updated_on: datetime, start_date: date, done_ratio: integer, estimated_hours: float, parent_id: integer, root_id: integer, lft: integer, rgt: integer, is_private: boolean)"
+  end
+
+  def test_access_journal_model
+    assert Journal.all.is? Array
+  end
+
+  def test_journal_structure
+    assert Journal.inspect == "Journal(id: integer, journalized_id: integer, journalized_type: string, user_id: integer, notes: text, created_on: datetime)"
+  end  
+
 end
