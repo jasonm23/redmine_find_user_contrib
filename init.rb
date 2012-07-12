4508da9e9e2f57658f622c999688893bdff4a208
Jason <jasonm23@gmail.com>
2012-07-12 11:52:30 +1000

    Update master

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_find_user_contrib do
   name 'Redmine Find User Contributed plugin'
   author 'Jason Milkins'
   description "Find issues belonging to a project, which a user has contributed to - responds in .json - usage: http://redmine_url/contributed.json?user_id={id}&project_id={id}&key={redmine api key}"
-  version '0.2.0'
+  version '0.1.2'
   url 'https://github.com/jasonm23/redmine_find_user_contrib'
   author_url 'https://github.com/jasonm23/'
   permission :find_contributed, :contributed => :find
