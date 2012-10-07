42e14706d1b52c69410fb8803844240f7bb9e9b0
jasonm23 <jasonm23@gmail.com>
2012-10-07 11:59:35 +1100

    made compatible with both plugin versions (both rails2 and rails3)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
-ActionController::Routing::Routes.draw do |map|
-  # /contributed.json?api_key=[key]&user_id=2&project_id=2
-  map.contributed_find '/contributed.json', :controller => 'contributed', :action => 'find'
-  map.contributed_find '/contributed.js', :controller => 'contributed', :action => 'find'
+if Rails::VERSION::MAJOR >= 3
+  RedmineApp::Application.routes.draw do
+    get '/contributed.json', :to => "contributed#find"
+    get '/contributed.js', :to => "contributed#find"
+  end
+else
+  ActionController::Routing::Routes.draw do |map|
+    # /contributed.json?api_key=[key]&user_id=2&project_id=2
+    map.contributed_find '/contributed.json', :controller => 'contributed', :action => 'find'
+    map.contributed_find '/contributed.js', :controller => 'contributed', :action => 'find'
+  end
 end
